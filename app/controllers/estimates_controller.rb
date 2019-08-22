f3026595a6dd1974429c0911823aaf59b47ca462
Ayumi Fudaba <vbmc66150@gmail.com>
2019-08-22 22:50:06 +0900

    Edit Estimate(controller,new,show)

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -1,5 +1,8 @@
 class EstimatesController < ApplicationController
   def index
+    @estimates = EstimateHeader.joins(:estimate_details, :customer)
+                    .select("estimate_headers.id, customers.customer_name,
+estimate_headers.customer_person, estimate_headers.estimate_id, estimate_details.product_name")
   end
 
   def new
@@ -8,10 +11,37 @@ class EstimatesController < ApplicationController
   end
 
   def create
-    @estimate = current_user.estimate_headers.new(params_int(estimate_params))
+    d = Date.today
+    d.strftime("%y%m%d")
+    @estimate = EstimateHeader.new
+    eh_param = estimate_params
+    @estimate.employee = current_user
+    @estimate.customer = Customer.find(eh_param[:customer].to_i)
+    @estimate.customer_person = eh_param[:customer_person]
+    # TODO Header:id が存在しない場合、1で登録する。
+    @estimate.estimate_id =
+        @estimate.customer.customer_initial + d.strftime + '−' + (EstimateHeader.last[:id] +1).to_s
+    if @estimate.save
 
-    if @estimate.save!
-      redirect_to estimates_path, notice: "見積「#{@estimate_h.estimate_id}」を登録しました"
+    else
+      render :new
+    end
+    @estimate_d = EstimateDetail.new
+    ed_param = estimate_params[:estimate_d]
+    @estimate_d.product_name = ed_param[:product_name]
+    @estimate_d.detail = ed_param[:detail]
+    @estimate_d.quantity = ed_param[:quantity].to_i
+    @estimate_d.kind = ed_param[:kind]
+    @estimate_d.unit_price = ed_param[:unit_price].to_i
+    @estimate_d.total_fee = @estimate_d.quantity * @estimate_d.unit_price
+    @estimate_d.tax = @estimate_d.total_fee * 0.1
+    @estimate_d.tax_amount = @estimate_d.total_fee + @estimate_d.tax
+    @estimate_d.vendor = Vendor.find(ed_param[:vendor].to_i)
+    @estimate_d.estimate_header_id = EstimateHeader.last[:id]
+    @estimate_d.estimate_detail_id = 1
+
+    if @estimate_d.save
+      redirect_to estimates_path, notice: "見積書「#{@estimate.estimate_id}」を登録しました"
     else
       render :new
     end
@@ -37,7 +67,7 @@ class EstimatesController < ApplicationController
   def estimate_params
     params.require(:estimate_header).permit(
         :customer, :customer_person, :estimate_id,
-        estimate_d_attributes: [:product_name, :detail, :quantity, :kind, :unit_price, :delivery_period, :vendor_id])
+        estimate_d: [:product_name, :detail, :quantity, :kind, :unit_price, :delivery_period, :vendor])
   end
 
   def params_int(estimate_params)
