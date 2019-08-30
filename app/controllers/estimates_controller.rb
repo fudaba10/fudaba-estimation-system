de8b92ec981de976aa7ce14d69cefb5baaae4719
Ayumi Fudaba <vbmc66150@gmail.com>
2019-08-30 20:45:49 +0900

    Edit Estimate(edit)

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -21,8 +21,14 @@ class EstimatesController < ApplicationController
     @estimate.customer = Customer.find(eh_param[:customer].to_i)
     @estimate.customer_person = eh_param[:customer_person]
     # TODO Header:id が存在しない場合、1で登録する。
+    eh_id = EstimateHeader.last
+     if eh_id.nil?
+       eh_id = '1'
+     else
+       eh_id = (EstimateHeader.last[:id] +1).to_s
+     end
     @estimate.estimate_id =
-        @estimate.customer.customer_initial + d.strftime + '−' + (EstimateHeader.last[:id] +1).to_s
+        @estimate.customer.customer_initial + d.strftime + '−' + eh_id
 
     @estimate_d = EstimateDetail.new
 
@@ -57,13 +63,34 @@ class EstimatesController < ApplicationController
   end
 
   def edit
-    estimate = EstimateHeader.includes({:estimate_details => :vendor}, :customer)
-                   .select("vendors.vendor_name, customers.customer_name,
-estimate_details.*, estimate_headers.*")
-                   .find(params[:id])
-    estimate.update!(estimate_params)
+    @estimate_h = EstimateHeader.find(params[:id])
+    @estimate_d = EstimateHeader.includes(:estimate_details).where(estimate_header_id: params[:id])
+  end
+
+  def update
+    estimate = EstimateHeader.find(params[:id])
+    eh_param = estimate_params
+    estimate.customer = Customer.find(eh_param[:customer].to_i)
+    estimate.customer_person = eh_param[:customer_person]
+
+    estimate_d = EstimateDetail.where(estimate_header_id: params[:id])
+    ed_param = estimate_params[:estimate_d]
+    estimate_d.product_name = ed_param[:product_name]
+    estimate_d.detail = ed_param[:detail]
+    estimate_d.quantity = ed_param[:quantity].to_i
+    estimate_d.kind = ed_param[:kind]
+    estimate_d.unit_price = ed_param[:unit_price].to_i
+    estimate_d.total_fee = estimate_d.quantity * estimate_d.unit_price
+    estimate_d.tax = estimate_d.total_fee * 0.1
+    estimate_d.tax_amount = estimate_d.total_fee + estimate_d.tax
+    estimate_d.vendor = Vendor.find(ed_param[:vendor].to_i)
+    estimate_d.estimate_detail_id = 1
+
+    estimate.save
+    estimate_d.save
     redirect_to estimates_url, notice: "見積書NO「#{estimate.estimate_id}」を更新しました。"
   end
+
 
   def destroy
     estimate = EstimateHeader.find(params[:id])
